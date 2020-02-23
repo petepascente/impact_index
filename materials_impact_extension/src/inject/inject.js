25ea05771fc8e45ed0dd2602d5bfb830a1e4820d
Alex Fiel <alfi1868@colorado.edu>
2020-02-22 21:31:01 -0700

    Cleaned up search function
    Condensed search

--- materials_impact_extension/src/inject/inject.js
+++ materials_impact_extension/src/inject/inject.js
@@ -3,13 +3,19 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		//console.log("Hello. This message was sent from scripts/inject.js");
+		console.log("Page Loaded.");
+		// ----------------------------------------------------------
+	}
+	}, 10);
+});
 
-		var searchFor = 'Emu';
+var materials = ["Polyester", "acrylic", "foo", "encyclopedia"];
 
+function searchForMaterials(materials){
+	for (i = 0; i < materials.length; i++) {
+		var searchFor = materials[i];
 		var swapWith = '<span class="foundIt">' + searchFor + '</span>';
 
 		$("*:contains(" + searchFor + "):not(:has(*))" ).each(function(){
@@ -17,50 +23,17 @@ chrome.extension.sendMessage({}, function(response) {
 			text = text.replace(searchFor, swapWith);
 			this.innerHTML = text;
 		});
-
-		$(".foundIt").hover(
-			function(event) {
-				// The mouse has entered the element, can reference the element via 'this'
-			},
-			function (event) {
-				// The mouse has left the element, can reference the element via 'this'
-			}
-		);
-		// ----------------------------------------------------------
-	}
-	}, 10);
-});
-
-var materials = ["polyester", "acrylic", "foo"];
-
-var button = document.createElement("button");
-button.className = "dankAssButton";
-button.innerHTML = "Hella";
-
-function makeBiggah(){
-	console.log(button.classList);
-	button.classList.add("bigButt");
-}
-
-function makeSmallah(){
-	button.className = "dankAssButton";
-}
-
-button.addEventListener("mouesover", makeBiggah);
-button.addEventListener("mouseout", makeSmallah);
-
-function searchForMaterials(){
-	for (i = 0; i < materials.length; i++) {
-		if ($("*:contains('" + materials[i] + "')" ).length > 0) {
-			console.log('I have found ' + materials[i]);
-		};
 	};
+	$(".foundIt").hover(
+		function(event) {
+			// The mouse has entered the element, can reference the element via 'this'
+		},
+		function (event) {
+			// The mouse has left the element, can reference the element via 'this'
+		}
+	);
+	console.log("Spans added.");
 };
 
-document.body.appendChild(button);
+searchForMaterials(materials);
 
-searchForMaterials();
-
-console.log($("*:contains(materials[i])" ));
-console.log($("*:contains(materials[i])" ).size);
-console.log($("*:contains(materials[i])" ).length);
